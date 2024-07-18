4bf291224ee4a6630dbdceaab3b5bdb899ce6c6c
ricogod <125234686+ricogod@users.noreply.github.com>
2024-07-18 14:43:36 +0700

    added extracted method to func2.go

--- controllers/func.go
+++ controllers/func.go
@@ -12,28 +12,6 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func GetItems(w http.ResponseWriter, r *http.Request) {
-	db := config.GetDB()
-	rows, err := db.Query("SELECT * FROM items")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	defer rows.Close()
-	items := []models.Item{}
-	for rows.Next() {
-		var item models.Item
-		err := rows.Scan(&item.ID, &item.Name, &item.Description, &item.Price)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		items = append(items, item)
-	}
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(items)
-}
-
 func GetItem(w http.ResponseWriter, r *http.Request) {
 	db := config.GetDB()
 	params := mux.Vars(r)
